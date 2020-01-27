609a9e2efa4f7ac0661078d4bfb6028947cdb11d
Jessica Katelyn Blomer <katelynblomer@gmail.com>
2020-01-27 12:02:24 -0700

    jan 27

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + "/index.html"));
+    
 });
+
+
+app.get("/about", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/public/about.html"));
+});
+
+app.get("/contact", (req,res)=>{
+    res.sendFile(path.join(__dirname) + "/public/contact.html");
+});
+
 
 app.listen(3000, ()=>{
     console.log("app is listening on port 3000");
